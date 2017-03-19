c0477ed4d3a4e9570133dd78db1b74c5329ab332
kengo hiwatashi <hixxxxxhi@45s-MBP.local>
2017-03-19 18:44:38 +0900

    Add stripe

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   has_many :listings
+  has_many :reservations
+
+  has_attached_file :image, :styles => { :medium => "400x400", :thumb => "100x100>" }, :default_url => "avatar-default.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   def self.from_omniauth(auth)
    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -13,5 +17,9 @@ class User < ApplicationRecord
      user.name = auth.info.name
      user.image = "http://graph.facebook.com/#{auth.uid}/picture?type=large"
    end
+  end
+
+  def connected?
+    !stripe_user_id.nil?
   end
 end
